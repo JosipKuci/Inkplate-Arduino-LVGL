f6c2bb7d5a9f1389a42385541b2f3b4d40e6516b
Josip Šimun Kuči <josipsimunkuci@gmail.com>
2025-11-10 12:56:55 +0100

    Add RGB565 color format if board is in color

--- src/lvgl/src/lv_conf.h
+++ src/lvgl/src/lv_conf.h
@@ -27,7 +27,11 @@
  *====================*/
 
 /** Color depth: 1 (I1), 8 (L8), 16 (RGB565), 24 (RGB888), 32 (XRGB8888) */
+#if !defined(ARDUINO_INKPLATECOLOR) && !defined(ARDUINO_INKPLATE2)
 #define LV_COLOR_DEPTH 8
+#else
+#define LV_COLOR_DEPTH 16
+#endif
 
 /*=========================
    STDLIB WRAPPER SETTINGS
